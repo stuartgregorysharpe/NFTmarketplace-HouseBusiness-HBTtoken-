bd2b9cdcc1506e589e065c1b974fe4d371c03739
phoenixdevguru <phoenixdevguru>
2023-05-04 14:52:42 -0600

    feat: implemented functions for token, ccontract smart contracts

--- contracts/Operator.sol
+++ contracts/Operator.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
+import '@openzeppelin/contracts/access/Ownable.sol';
+
 interface IHouseBusiness {
     function setPayable(uint256 tokenId, address _buyer, bool nftPayable) external;
 
@@ -55,24 +57,87 @@ interface IHouseBusiness {
     function changeHousePrice(uint256 tokenId, uint256 newPrice) external;
 }
 
-interface IMainCleanContract {}
+interface IMainCleanContract {
+    // write Contract
+    function ccCreation(
+        string memory _companyName,
+        string memory _contractType,
+        address _contractSigner,
+        string memory _contractURI,
+        uint256 _dateFrom,
+        uint256 _dateTo,
+        uint256 _agreedPrice,
+        string memory _currency
+    ) external;
 
-interface IHouseBusinessToken {}
+    // Add Contract Signer
+    function addContractSigner(uint256 _ccID, address _contractSigner) external;
 
-contract Operator {
+    // sign contract
+    function signContract(uint256 ccID) external;
+
+    // send sign notification
+    function sendNotify(address _notifyReceiver, string memory _notifyContent, uint256 ccID) external;
+
+    /**
+     * @dev modifies ownership of `contractId` from `from` to `to`
+     */
+    function transferContractOwnership(uint256 contractId, address from, address to) external;
+}
+
+interface IHouseBusinessToken {
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool);
+}
+
+contract Operator is Ownable {
     IHouseBusiness HouseBusiness;
     IMainCleanContract CContract;
     IHouseBusinessToken ERC20Token;
 
     // Utility tokens and NFT address
     address houseBusiness = 0x0A964d282AF35e81Ad9d72e5c215108B3c43D3c1;
-    address cContractAddress = 0xaa3Dc2E3ca0FE2dE6E519F0F224456861A7e9cFC;
+    address cContract = 0xaa3Dc2E3ca0FE2dE6E519F0F224456861A7e9cFC;
     address erc20Token = 0x27C1F4539Fd2CcE5394Ea11fA8554937A587d684;
 
     constructor() {
         // init contract interfaces
         HouseBusiness = IHouseBusiness(houseBusiness);
-        CContract = IMainCleanContract(cContractAddress);
+        CContract = IMainCleanContract(cContract);
         ERC20Token = IHouseBusinessToken(erc20Token);
+    }
+
+    /**
+     * Provides the ability to update smart contract addresses for scalability.
+     * @param _houseBusiness HouseBusiness NFT address
+     */
+    function setHouseBusiness(address _houseBusiness) public onlyOwner {
+        houseBusiness = _houseBusiness;
+        HouseBusiness = IHouseBusiness(_houseBusiness);
+    }
+
+    /**
+     * Provides the ability to update smart contract addresses for scalability.
+     * @param _cContract MainCleanContract address
+     */
+    function setCContract(address _cContract) public onlyOwner {
+        cContract = _cContract;
+        CContract = IMainCleanContract(_cContract);
+    }
+
+    /**
+     * Provides the ability to update smart contract addresses for scalability.
+     * @param _erc20Token HouseBusinessToken address
+     */
+    function setERC20Token(address _erc20Token) public onlyOwner {
+        erc20Token = _erc20Token;
+        ERC20Token = IHouseBusinessToken(_erc20Token);
     }
 }
