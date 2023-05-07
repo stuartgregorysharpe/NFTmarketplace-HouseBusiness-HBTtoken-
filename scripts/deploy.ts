3ead68aa8b204a8c3ba2c8799f9c9508b9e0db8b
phoenixdevguru <phoenixdevguru>
2023-05-07 10:22:05 -0600

    verify Operator smart contract on mumbai testnet

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -18,7 +18,7 @@ async function main() {
   console.log('Starting deployments');
   const accounts = await hre.ethers.getSigners();
   const deployer = accounts[0];
-  const tokenAddress = '0xd0d178ed60436d0d1762ac58484eea154946edcd'; 
+  const tokenAddress = '0x65079701914EA470Fc8f3Fc50D0ef782CaDd709D';
 
   const tokenFactory = await ethers.getContractFactory('HouseBusinessToken');
   const House = (await tokenFactory.deploy()) as HouseBusinessToken;
@@ -46,6 +46,11 @@ async function main() {
   await ThirdPartyContract.deployed();
   console.log('This is the third party address; ', ThirdPartyContract.address);
 
+  const operatorFactory = await ethers.getContractFactory('Operator');
+  const Operator = (await operatorFactory.deploy(tokenAddress));
+  await Operator.deployed();
+  console.log('This is the token address: ', Operator.address);
+
   let tx = await HouseNFT.connect(deployer).setCContractAddress(CContract.address);
   await tx.wait();
 
@@ -63,6 +68,7 @@ async function main() {
   writeAddr(addressFile, network.name, House.address, 'ERC-20');
   writeAddr(addressFile, network.name, HouseNFT.address, 'HouseNFT');
   writeAddr(addressFile, network.name, CContract.address, 'CleanContract');
+  writeAddr(addressFile, network.name, Operator.address, 'OperatorContract');
 
   console.log('Deployments done, waiting for etherscan verifications');
 
@@ -72,6 +78,7 @@ async function main() {
   await verify(HouseNFT.address, [House.address]);
   await verify(CContract.address, [HouseNFT.addesrs]);
   await verify(StakingContract.address, [HouseNFT.address, House.address]);
+  await verify(Operator.address, [tokenAddress]);
 
   console.log('All done');
 }
