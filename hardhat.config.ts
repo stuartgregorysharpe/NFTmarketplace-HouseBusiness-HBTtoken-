e3e6908cbb734e63dea98c17afe22be4a807d186
phoenixdevguru <phoenixdevguru>
2023-05-06 20:34:37 -0600

    deploy Operator again, getting issue while verifying the contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,6 @@
 require('@nomiclabs/hardhat-ethers');
 require('@openzeppelin/hardhat-upgrades');
 import '@nomiclabs/hardhat-waffle';
-// import 'hardhat-gas-reporter';
 import 'hardhat-contract-sizer';
 import 'solidity-coverage'
 import dotenv from 'dotenv';
@@ -9,9 +8,7 @@ dotenv.config();
 
 const privateKey = process.env.PRIVATE_KEY;
 const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+
 module.exports = {
   solidity: {
     version: '0.8.7',
@@ -22,7 +19,6 @@ module.exports = {
       },
     },
   },
-  // defaultNetwork: 'goerli',
   networks: {
     goerli: {
       url: "https://goerli.infura.io/v3/0e42c582d71b4ba5a8750f688fce07da",
@@ -37,17 +33,7 @@ module.exports = {
       timeout: 100000
     },
   },
-  // mocha: {
-  //   timeout: 400000,
-  // },
-  // gasReporter: {
-  //   enabled: false,
-  //   currency: 'USD',
-  // },
-  // contractSizer: {
-  //   runOnCompile: false,
-  // },
   etherscan: {
-    apiKey: process.env.ETHER_KEY
+    apiKey: process.env.ETHERSCAN_API_KEY
   },
 };
